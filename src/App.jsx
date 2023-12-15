ba709383acd689bfbe536c0c9e6e7d27aa737665
abhijeet0892 <abhijeetchougule1008@gmail.com>
2023-12-16 01:30:44 +0530

    Using tailwind css create password generator

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PasswordGen from "./components/PasswordGen";
+
+function App() {
+  return (
+    <div>
+      <PasswordGen />
+    </div>
+  );
+}
+
+export default App;
